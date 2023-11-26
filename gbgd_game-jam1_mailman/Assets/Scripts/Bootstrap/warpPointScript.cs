0127d082c1066b1e5e3932b4dbbc55ea7ff9cc60
manateabags <manateabagsart@gmail.com>
2023-11-25 23:33:09 -0800

    set up a testing environment and set up warp point tool (still need to test tho)

---
+++ gbgd_game-jam1_mailman/Assets/Scripts/Bootstrap/warpPointScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warpPointScript : MonoBehaviour
+{
+    public int parentTransform;
+    public bool repeats;
+    public int numberOfTimesRepeated;
+
+}
